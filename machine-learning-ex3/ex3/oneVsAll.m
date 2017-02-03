7971c0beac145a09d98c3e77ac4025dc905049d6
Susumu Hoshino <Susumu Hoshino>
2017-02-04 01:15:39 +0900

    ml ex3 oneVSAll

--- machine-learning-ex3/ex3/oneVsAll.m
+++ machine-learning-ex3/ex3/oneVsAll.m
@@ -49,11 +49,20 @@ X = [ones(m, 1) X];
 %                 initial_theta, options);
 %
 
+for i = 1:num_labels
 
+% Set Initial theta
+initial_theta = zeros(n + 1, 1);
+% Set options for fminunc
+options = optimset('GradObj', 'on', 'MaxIter', 50);
 
+arr = 1:num_labels;
 
+label = (arr == y);
 
+all_theta(i,:) = fmincg (@(t)(lrCostFunction(t, X, label(:,i), lambda)),initial_theta, options);
 
+end
 
 
 
